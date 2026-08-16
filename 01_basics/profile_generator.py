3b67f41e2ffc7c8e44a028afb72903826394adb6
Ravi Chandra Thapa <noob.ravi@gmail.com>
2026-08-16 11:05:09 +0545

    Add .gitignore and Day 1 profile generator script

---
+++ 01_basics/profile_generator.py
@@ -0,0 +1,19 @@
+user_name = input("Please enter name: ")
+user_age = int(input("Enter age: "))
+user_country = input("Enter Country: ")
+user_occupation = input("Enter occupation: ")
+programming_language = input("Enter favourite programming Language: ")
+birth_year = 2026-user_age
+
+print("=" * 40)
+print("PERSONAL PROFILE")
+print("=" * 40)
+
+print(f"Name: {user_name}")
+print(f"Age: {user_age}")
+print(f"Country: {user_country}")
+print(f"Occupation: {user_occupation}")
+print(f"Favourite Programming Language: {programming_language}")
+print(f"Birth Year: {birth_year}")
+
+print("=" * 40)
